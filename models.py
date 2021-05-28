5e78f636d9ee1d6427d963755d9958cd87e8167d
Viet Nguyen <vietnguyen@viets-mbp.home>
2021-05-27 19:15:18 -0700

    start

---
+++ models.py
@@ -0,0 +1,3 @@
+import layers
+import torch
+import torch.nn as nn
---
+++ train.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.optim.lr_scheduler as sc
+
+def main():
+	print(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
