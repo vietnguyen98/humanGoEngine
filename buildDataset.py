dff9eddc4dc1187458c9bd5232421d522a4b093c
Viet Nguyen <vietnguyen@viets-mbp.home>
2021-05-27 19:22:31 -0700

    dataset build

---
+++ buildDataset.py
@@ -0,0 +1,24 @@
+import sys
+import random
+import os
+import numpy as np
+import pandas as pd
+import torch
+from tqdm.notebook import tqdm as tq
+from datetime import datetime as dt
+import glob
+from collections import Counter
+from sgfmill import sgf
+from sgfmill import boards
+from sgfmill import ascii_boards
+from tqdm import tqdm
+
+
+def main():
+	print(sys.argv)
+	for i in tqdm(range(10)):
+		print(i)
+
+
+if __name__ == '__main__':
+    main()
